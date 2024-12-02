0a385e307c55ac50351ba1c130a448b7583740f0
durga prasad <durgaprasad@isthara.com>
2024-12-02 09:33:29 +0530

    updated dist bundle

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './dist/enquiry-form/fesm2022/enquiry-form.mjs', // Adjust the path to your compiled library
+  output: {
+    path: path.resolve(__dirname, 'dist-bundle'),
+    filename: 'enquiry-form.bundle.js',
+    library: 'AppointmentLib', // This defines the global object
+    libraryTarget: 'umd', // UMD to support various module systems and global scope
+    umdNamedDefine: true, // Name the AMD module, helpful in global contexts
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
