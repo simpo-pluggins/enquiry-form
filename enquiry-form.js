b9afc44d5f123b00544dead7bfd6cdaf1f7e5cd8
simpo-ai <160307682+simpo-ai@users.noreply.github.com>
2024-12-06 15:38:43 +0530

    Update enquiry-form.js

--- enquiry-form.js
+++ enquiry-form.js
@@ -3,7 +3,6 @@ var accessKey ;
 class getEnquiryForm{
     constructor() {
         window.onload = function(){
-        // setTimeout(() => {
                 if(document.getElementById('enquiry-form') != null){
                 let enquiryId = document.getElementById('enquiry-form');
                 let dataId;
@@ -15,15 +14,11 @@ class getEnquiryForm{
                 const encodedFontFamily = encodeURIComponent(fontFamily);
                 const id = encodeURIComponent(accessKey.split('=')[0]);
                 const bId = encodeURIComponent(accessKey.split('=')[1]);
-                localStorage.setItem('bId',accessKey.split('=')[1])
-                localStorage.setItem('denv',dataId)
-                const url = `https://simpo-pluggins.github.io/enquiry-form/index.html?id=${id}&ff=${fontFamily}&denv=${dataId}&bId=${bId}`;
                 if (!enquiryId) return;
                 enquiryId.innerHTML += `
                 <iframe id="myHtml" src="https://simpo-pluggins.github.io/enquiry-form/index.html?id=${id}&ff=${encodedFontFamily}&denv=${dataId}&bId=${bId}" style="width:100%;height:calc(100vh - 20px);border:none;"></iframe>
                 `
             }
-            // }, 200);
         }
     }
 }
